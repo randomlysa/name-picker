e4769083d49889d5e0e2c82e88840a33d36edaf0
randomlysa <su@randomlysa.com>
2019-02-07 16:38:13 -0600

    move logic into app
    so results can be displayed in results.js

--- src/app.js
+++ src/app.js
@@ -9,15 +9,42 @@ class App extends React.Component {
   state = {
     min: 0,
     max: 10,
-    haveLetters: [],
-    notHaveLetters: []
+    letters: []
   };
 
-  componentDidMount() {}
+  handleChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let { min, max, letters } = this.state;
+    // Query won't work without something for letters.
+    if (!min) min = 0;
+    if (!max) max = 10;
+    if (letters.length === 0) letters = ['_____'];
+
+    const results = axios.get(
+      `http://localhost:3000/search/${min}/${max}/${letters}`
+    );
+    results.then(r => {
+      console.log(r.data);
+      // r.data.forEach(name => {
+      //   console.log(name, name.length);
+      // });
+    });
+  };
+
   render() {
     return (
       <div>
-        <Filters />
+        <Filters
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          min={this.state.min}
+          max={this.state.max}
+          letters={this.state.letters}
+        />
         React
       </div>
     );
--- src/filters.js
+++ src/filters.js
@@ -4,51 +4,28 @@ import axios from 'axios';
 export default class Filters extends React.Component {
   // Search by letters:
   // https://stackoverflow.com/a/1580104/3996097
-  state = {
-    min: 0,
-    max: 10,
-    letters: []
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    let { min, max, letters } = this.state;
-    // Query won't work without something for letters.
-    if (!min) min = 0;
-    if (!max) max = 10;
-    if (letters.length === 0) letters = ['_____'];
-
-    const results = axios.get(
-      `http://localhost:3000/search/${min}/${max}/${letters}`
-    );
-    results.then(r => console.log(r.data));
-  };
-
-  handleChange = e => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
 
   render() {
     return (
       <React.Fragment>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.props.handleSubmit}>
           <input
             placeholder="Min Length"
             id="min"
-            value={this.state.min}
-            onChange={this.handleChange}
+            value={this.props.min}
+            onChange={this.props.handleChange}
           />
           <input
             placeholder="Max Length"
             id="max"
-            value={this.state.max}
-            onChange={this.handleChange}
+            value={this.props.max}
+            onChange={this.props.handleChange}
           />
           <input
             placeholder="Letters"
             id="letters"
-            value={this.state.letters}
-            onChange={this.handleChange}
+            value={this.props.letters}
+            onChange={this.props.handleChange}
           />
           <input type="submit" />
         </form>
