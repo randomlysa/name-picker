745253435a48520b0ab43ba477ffbe60c223a5eb
randomlysa <su@randomlysa.com>
2019-02-07 19:24:53 -0600

    in saved list, click to remove name

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Saved saved={this.state.saved} />
+        <Saved saved={this.state.saved} toggleSaveName={this.toggleSaveName} />
         <Filters
           handleSubmit={this.handleSubmit}
           handleChange={this.handleChange}
--- src/saved.js
+++ src/saved.js
@@ -8,8 +8,36 @@ const Fixed = styled.div`
   padding: 1em;
 `;
 
+const Name = styled.button`
+  margin-right: 1em;
+  background-color: #dff3e3;
+  cursor: pointer;
+  padding: 5px;
+  border: none;
+  font-family: inherit;
+  font-size: 1.3em;
+  transition: all 0.3s;
+
+  :hover {
+    background-color: #ce7da5;
+  }
+`;
+
 export default class Saved extends React.Component {
   render() {
-    return <Fixed>{this.props.saved.join(', ')}</Fixed>;
+    return (
+      <Fixed>
+        {this.props.saved.map(name => {
+          return (
+            <Name
+              key={name}
+              onClick={this.props.toggleSaveName.bind(this, name)}
+            >
+              {name}
+            </Name>
+          );
+        })}
+      </Fixed>
+    );
   }
 }
