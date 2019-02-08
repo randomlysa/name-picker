7233cb69b7bf05bfa67f76eab89049029cab9d9f
randomlysa <su@randomlysa.com>
2019-02-07 19:16:46 -0600

    make input sizes smaller using css

--- src/filters.js
+++ src/filters.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const StyledInput = styled.input`
+  width: 5em;
+`;
 
 export default class Filters extends React.Component {
   // Search by letters:
@@ -8,19 +13,23 @@ export default class Filters extends React.Component {
     return (
       <React.Fragment>
         <form onSubmit={this.props.handleSubmit}>
-          <input
+          <StyledInput
+            size="3"
+            type="number"
             placeholder="Min Length"
             id="min"
             value={this.props.min}
             onChange={this.props.handleChange}
           />
-          <input
+          <StyledInput
+            size="3"
+            type="number"
             placeholder="Max Length"
             id="max"
             value={this.props.max}
             onChange={this.props.handleChange}
           />
-          <input
+          <StyledInput
             placeholder="Letters"
             id="letters"
             value={this.props.letters}
