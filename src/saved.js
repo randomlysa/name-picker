418acf66f4865933326dcf70b0a51bac2ba7277f
randomlysa <su@randomlysa.com>
2019-02-07 19:17:01 -0600

    attached saved names to bottom

--- src/saved.js
+++ src/saved.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Fixed = styled.div`
+  position: fixed;
+  bottom: 0;
+  background-color: #dff3e3;
+  padding: 1em;
+`;
 
 export default class Saved extends React.Component {
   render() {
-    return this.props.saved.join(', ');
+    return <Fixed>{this.props.saved.join(', ')}</Fixed>;
   }
 }
