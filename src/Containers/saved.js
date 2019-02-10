e63188a4913969f43ad8103729dbaf9aeae44d2d
randomlysa <su@randomlysa.com>
2019-02-09 18:47:26 -0600

    add button to toggle saved names list

--- src/Containers/saved.js
+++ src/Containers/saved.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import ListIcon from '../img/iconmonstr-menu-4-24.png';
+
 const SavedListContainer = styled.div`
   position: fixed;
-  bottom: 0;
-  background-color: #dff3e3;
   padding: 1em;
-  width: 100%;
+  bottom: 0;
+  background-color: ${props => (props.width ? '#dff3e3' : 'none')};
+  width: ${props => (props.width ? '100%' : '0px')};
+`;
+
+/* LabelText = 'Click to remove' */
+const ListWithLabelText = styled.div`
+  display: ${props => (props.display ? 'block' : 'none')};
+`;
+
+const ToggleSavedListButton = styled.button`
+  background: none;
+  border: none;
+  border-radius: 100px;
+  margin-right: 1em;
+  padding: 1em;
+  cursor: pointer;
+  float: left;
+
+  :hover {
+    background: #ffd1ba;
+  }
 `;
 
 const Name = styled.button`
@@ -25,20 +46,33 @@ const Name = styled.button`
 `;
 
 export default class Saved extends React.Component {
+  state = { showListWithLabel: true };
+
+  toggleDisplayListWithLabel = () => {
+    this.setState(state => {
+      return { showListWithLabel: !state.showListWithLabel };
+    });
+  };
+
   render() {
     return (
-      <SavedListContainer>
-        Click to remove:
-        {this.props.saved.map(name => {
-          return (
-            <Name
-              key={name}
-              onClick={this.props.toggleSaveName.bind(this, name)}
-            >
-              {name}
-            </Name>
-          );
-        })}
+      <SavedListContainer width={this.state.showListWithLabel ? 1 : 0}>
+        <ToggleSavedListButton onClick={this.toggleDisplayListWithLabel}>
+          <img src={ListIcon} />
+        </ToggleSavedListButton>
+        <ListWithLabelText display={this.state.showListWithLabel ? 1 : 0}>
+          Click to remove:
+          {this.props.saved.map(name => {
+            return (
+              <Name
+                key={name}
+                onClick={this.props.toggleSaveName.bind(this, name)}
+              >
+                {name}
+              </Name>
+            );
+          })}
+        </ListWithLabelText>
       </SavedListContainer>
     );
   }
