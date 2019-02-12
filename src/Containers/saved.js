a82b7a5ff039cf306ed478133a2dc863a99834a6
randomlysa <su@randomlysa.com>
2019-02-11 18:12:02 -0600

    add a button to toggle showing full list of saved names

--- src/Containers/saved.js
+++ src/Containers/saved.js
@@ -16,6 +16,12 @@ const ListWithLabelText = styled.div`
   display: ${props => (props.display ? 'block' : 'none')};
 `;
 
+const ToggleShowFullListButton = styled.button`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+`;
+
 const ToggleSavedListButton = styled.button`
   background: none;
   border: none;
@@ -46,12 +52,42 @@ const Name = styled.button`
 `;
 
 export default class Saved extends React.Component {
-  state = { showListWithLabel: true };
+  state = { showListWithLabel: true, showFullList: false };
+  listLength = 2;
 
   toggleDisplayListWithLabel = () => {
     this.setState(state => {
       return { showListWithLabel: !state.showListWithLabel };
     });
+  };
+
+  renderNamesList = (name, index) => {
+    if (index <= this.listLength || this.state.showFullList === true) {
+      return (
+        <Name key={name} onClick={this.props.toggleSaveName.bind(this, name)}>
+          {name}
+        </Name>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  toggleShowFullList = () => {
+    this.setState(state => {
+      return { showFullList: !state.showFullList };
+    });
+  };
+
+  // show '...' button that toggles whether to show the full list of saved names
+  renderShowMoreButton = () => {
+    if (this.props.saved.length > 3) {
+      return (
+        <ToggleShowFullListButton onClick={this.toggleShowFullList}>
+          ...
+        </ToggleShowFullListButton>
+      );
+    }
   };
 
   render() {
@@ -62,16 +98,11 @@ export default class Saved extends React.Component {
         </ToggleSavedListButton>
         <ListWithLabelText display={this.state.showListWithLabel ? 1 : 0}>
           {this.props.saved.length === 1 && 'Click to remove:'}
-          {this.props.saved.map(name => {
-            return (
-              <Name
-                key={name}
-                onClick={this.props.toggleSaveName.bind(this, name)}
-              >
-                {name}
-              </Name>
-            );
+
+          {this.props.saved.map((name, index) => {
+            return this.renderNamesList(name, index);
           })}
+          {this.renderShowMoreButton()}
         </ListWithLabelText>
       </SavedListContainer>
     );
