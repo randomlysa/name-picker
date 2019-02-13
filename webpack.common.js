3c2b5a84dd132ba5a91480010d07bdfb5e617135
randomlysa <su@randomlysa.com>
2019-02-12 20:29:15 -0600

    add Bundle Analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
@@ -30,9 +32,10 @@ module.exports = {
       }
     ]
   },
-
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new BundleAnalyzerPlugin()]
 };
